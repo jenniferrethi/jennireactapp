357f4f0166e47cf1e5989b2f2bc12713b092bc0b
Jennifer Rethi <jenniferrethi@Jennifers-MBP.attlocal.net>
2020-10-30 16:43:35 -0500

    react tic tac toe

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 import time
 from flask import Flask
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    today = datetime.now()
+    return {'time': today}
